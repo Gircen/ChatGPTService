fb2c48ce1ba786dd58200a1c4735b8ac88b7aed0
antonkolesnikov <girrcen666@icloud.com>
2023-02-07 05:23:34 -0800

    rename Messages entities

--- src/main/java/org/chat/gpt/database/entity/MessageInboxImpl.java
+++ src/main/java/org/chat/gpt/database/entity/MessageInboxImpl.java
@@ -1,27 +1,23 @@
 package org.chat.gpt.database.entity;
 
+
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.util.UUID;
 
 @Setter
 @Entity
-@Table(name = "message_outbox", catalog = "chat_gpt")
-public class MessageOutbox extends Message{
+@Table(name = "message_inbox", catalog = "chat_gpt")
+public class MessageInboxImpl extends Message{
 
-    @Id
     Long id;
-
     @Column(columnDefinition = "uuid")
     UUID uuid;
     @Column(columnDefinition = "varchar")
     String message;
-
-    @ManyToOne
-    @JoinColumn(name = "message_inbox_id")
-    @Column(columnDefinition = "integer")
-    MessageInbox inbox;
 
     @javax.persistence.Id
     public Long getId() {
--- src/main/java/org/chat/gpt/database/entity/MessageOutboxImpl.java
+++ src/main/java/org/chat/gpt/database/entity/MessageOutboxImpl.java
@@ -1,23 +1,27 @@
 package org.chat.gpt.database.entity;
 
-
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.UUID;
 
 @Setter
 @Entity
-@Table(name = "message_inbox", catalog = "chat_gpt")
-public class MessageInbox extends Message{
+@Table(name = "message_outbox", catalog = "chat_gpt")
+public class MessageOutboxImpl extends Message{
 
+    @Id
     Long id;
+
     @Column(columnDefinition = "uuid")
     UUID uuid;
     @Column(columnDefinition = "varchar")
     String message;
+
+    @ManyToOne
+    @JoinColumn(name = "message_inbox_id")
+    @Column(columnDefinition = "integer")
+    MessageInboxImpl inbox;
 
     @javax.persistence.Id
     public Long getId() {
